c5cffaad47b2c9fb77db5eec38da846cc3ebf864
jaredCR13 <jaredmor78@gmail.com>
2025-04-25 14:46:16 -0600

    cambios

--- src/main/java/controller/ArithmeticConverterController.java
+++ src/main/java/controller/ArithmeticConverterController.java
@@ -1,10 +1,9 @@
 package controller;
 
+import domain.StackException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class ArithmeticConverterController {
 
@@ -25,19 +24,45 @@ public class ArithmeticConverterController {
 
     @FXML
     private TextField textField;
+    @FXML
+    private Label labelPrefix;
 
     @FXML
-    public void initialize(){
+    private Label labelPostfix;
 
+    @FXML
+    private ToggleGroup arithmeticConverterTypes;
+    @FXML
+    public void initialize(){
+        arithmeticConverterTypes= new ToggleGroup();
+        prefixRadioButton.setToggleGroup(arithmeticConverterTypes);
+        infixRadioButton.setToggleGroup(arithmeticConverterTypes);
+        postfixRadioButton.setToggleGroup(arithmeticConverterTypes);
     }
     @FXML
     void cleanOnAction(ActionEvent event) {
-
+        textField.clear();
+        textAreaPostfix.clear();
+        textAreaPrefix.clear();
     }
 
     @FXML
     void convertOnAction(ActionEvent event) {
-
+        try {
+            if (infixRadioButton.isSelected()) {
+                String resultPost = util.Utility.infixToPostfixConverter(textField.getText());
+                String resultPre = util.Utility.infixToPrefix(textField.getText());
+                textAreaPrefix.setText(resultPre);
+                textAreaPostfix.setText(resultPost);
+            }else if(postfixRadioButton.isSelected()){
+                String resultPre = util.Utility.posFixToPrefix(textField.getText());
+                String resultInfix = util.Utility.postfixToInfixConverter(textField.getText());
+                textAreaPrefix.setText(resultPre);
+                textAreaPostfix.setText(resultInfix);
+            }
+        }catch (StackException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/controller/BaseConverterController.java
+++ src/main/java/controller/BaseConverterController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import domain.StackException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class BaseConverterController {
 
@@ -24,18 +26,37 @@ public class BaseConverterController {
     private TextField textField;
 
     @FXML
-    public void initialize(){
+    private ToggleGroup numberType;
 
+    @FXML
+    public void initialize(){
+        numberType = new ToggleGroup();
+        binaryRadioButton.setToggleGroup(numberType);
+        octalRadioButton.setToggleGroup(numberType);
+        hexaRadioButton.setToggleGroup(numberType);
 
     }
     @FXML
     void clearOnAction(ActionEvent event) {
-
+        textField.clear();
+        textArea.clear();
     }
 
     @FXML
     void convertOnAction(ActionEvent event) {
-
+        try {
+            if (binaryRadioButton.isSelected()) {
+                String result = util.Utility.decimalToBinary(Integer.parseInt(textField.getText()));
+                textArea.setText(result);
+            } else if (octalRadioButton.isSelected()) {
+                String result = util.Utility.decimalToOctal(Integer.parseInt(textField.getText()));
+                textArea.setText(result);
+            } else if (hexaRadioButton.isSelected()) {
+                String result = util.Utility.decimalToHexaDecimal(Integer.parseInt(textField.getText()));
+                textArea.setText(result);
+            }
+        } catch (StackException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
